6b4e46e4c2d0a5939ce14700d71bf91c90c98a18
DanielElibar11 <danielibar72@gmail.com>
2024-07-24 18:41:16 -0600

    first commit

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2023 <a href="https://www.instagram.com/daniel_eliruz/" target=" blank">BETAS</a>.
+	</strong>
+
+	Todos los derechos reservados.
+
+</footer>
