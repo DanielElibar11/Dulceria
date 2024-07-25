e10df8868e4b77acd21dc5b62317aa4a1921d104
DanielElibar11 <144507020+DanielElibar11@users.noreply.github.com>
2024-07-24 19:47:27 -0600

    Update conexion.php
    4

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	public static function conectar(){
 
-		$link = new PDO('mysql:host=localhost;port=3306;dbname=dulceria_db', 'dulceria', 'nyc.domcloud.co');
+		$link = new PDO('mysql:host=localhost;dbname=dulceria_db', 'root', 'nyc.domcloud.co');
 
 		$link->exec("set names utf8");
 
